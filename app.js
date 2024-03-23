d6354e3b2b6e2740db77048bfd63ce8432d1920f
Mehraj <sisakilcste18@gmail.com>
2024-03-23 18:00:35 +0600

    nested html element implement with react

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-const heading = React.createElement(
-  "h1",
-  { id: "header" },
-  "Hello World from React!"
-);
 
-console.log(heading) // heading is a js object.
+/*
+Creating nested element.
+
+<div id="parent">
+    <div id="child1">
+        <h1> I'm h1 </h1>
+        <h2> I'm h2 </h2>
+    </div>
+    <div id="child2">
+        <h1> I'm h1 </h1>
+        <h2> I'm h2 </h2>
+    </div>
+</div>
+
+ReactElement(object) => (when render), HTML(Browser understands)  
+
+This is just a massy solution to creating just this nested html elements.
+
+To sovle this, 
+React come with "JSX" 
+Don't know what it "JSX", let's move into next.
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm h1 heading"),
+    React.createElement("h2", {}, "I'm h2 heading"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 heading"),
+    React.createElement("h2", {}, "I'm h2 heading"),
+  ]),
+]);
+
+
+
+console.log(parent) // heading is a js object.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
